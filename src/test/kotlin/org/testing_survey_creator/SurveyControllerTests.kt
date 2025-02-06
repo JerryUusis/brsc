e8ec2d5cbacc7c2ad585b9e2d32e26fa15b00af4
Jerry Uusitalo <jerry.uusitalo@gmail.com>
2025-02-06 13:50:50 +0200

    Add comments and refactor constructor

--- src/test/kotlin/org/testing_survey_creator/SurveyControllerTests.kt
+++ src/test/kotlin/org/testing_survey_creator/SurveyControllerTests.kt
@@ -4,23 +4,23 @@ import com.jayway.jsonpath.DocumentContext
 import com.jayway.jsonpath.JsonPath
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest
 import org.assertj.core.api.Assertions.assertThat
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.http.*
 import org.springframework.test.context.ActiveProfiles
 import org.testing_survey_creator.model.SurveyDTO
-import org.testing_survey_creator.repository.SurveyRepository
 import org.testing_survey_creator.service.SurveyService
 import java.net.URI
 
 // https://spring.io/guides/tutorials/spring-boot-kotlin
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-class SurveyControllerIntegrationTests @Autowired constructor(
-    private val restTemplate: TestRestTemplate,
-) {
+class SurveyControllerIntegrationTests {
+
+    @Autowired
+    private lateinit var restTemplate: TestRestTemplate
 
     @Autowired
     private lateinit var surveyService: SurveyService
@@ -33,25 +33,24 @@ class SurveyControllerIntegrationTests @Autowired constructor(
         assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
         assertThat(response.body).isNotEmpty()
 
-        // Parse response from JSON string to JSON document
+        // Parse JSON response body into a queryable JSON document so it can be queried
         val documentContext: DocumentContext = JsonPath.parse(response.body)
-        // Query with JSONPath expressions
-        // https://github.com/json-path/JsonPath?tab=readme-ov-file#operators
 
         // Convert LinkedHashMap list into List<SurveyDTO>
-        val actualSurveys: List<SurveyDTO> = documentContext.read<List<Map<String, Any>>>("$")
-            .map { map ->
-                SurveyDTO(
-                    issueNumber = (map["issueNumber"] as Number).toInt(),
-                    issueLink = map["issueLink"] as String,
-                    taskNumber = (map["taskNumber"] as Number).toInt(),
-                    taskTitle = map["taskTitle"] as String,
-                    instructions = map["instructions"] as List<String>
-                )
-            }
+        // https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/map.html
+        val actualSurveys: List<SurveyDTO> =
+            documentContext.read<List<Map<String, Any>>>("$") // Each JSON object is stored as a LinkedHashMap<String, Any> in a List<Map<String, Any>>
+                .map { map ->
+                    SurveyDTO(
+                        issueNumber = (map["issueNumber"] as Number).toInt(), // map example = "issueNumber" to 124
+                        issueLink = map["issueLink"] as String, // map example "issueLink" to "www.example.com"
+                        taskNumber = (map["taskNumber"] as Number).toInt(),
+                        taskTitle = map["taskTitle"] as String,
+                        instructions = (map["instructions"] as List<*>).filterIsInstance<String>() // Removes non-Strings https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.sequences/filter-is-instance.html
+                    )
+                }
 
         val expectedSurveys = surveyService.getAllSurveys()
-
         assertThat(actualSurveys.size).isEqualTo(expectedSurveys.size)
 
         // Loop through expected surveys array and assert them equal with actual surveys
@@ -71,7 +70,8 @@ class SurveyControllerIntegrationTests @Autowired constructor(
 
         val documentContext: DocumentContext = JsonPath.parse(response.body)
 
-        // Extract actual response data
+        // Extract actual response data with JSONPath Query expressions
+        // https://github.com/json-path/JsonPath?tab=readme-ov-file#operators
         val actualSurvey = SurveyDTO(
             issueNumber = documentContext.read("$.issueNumber"),
             issueLink = documentContext.read("$.issueLink"),
@@ -79,12 +79,11 @@ class SurveyControllerIntegrationTests @Autowired constructor(
             taskTitle = documentContext.read("$.taskTitle"),
             instructions = documentContext.read("$.instructions"),
         )
-
         assertThat(actualSurvey).isEqualTo(expectedSurvey)
     }
 
     @Test
-    fun `Post should return created and IncludeLocation`() {
+    fun `Post should return created and include location in location header`() {
         val newSurvey = SurveyDTO(
             issueNumber = 126,
             issueLink = "https://www.example3.com",
@@ -100,18 +99,21 @@ class SurveyControllerIntegrationTests @Autowired constructor(
         val actualLocation = response.headers.location
         assertThat(actualLocation).isNotNull()
 
+        // Extract the id from the location header of response object
         val surveyId = actualLocation!!.path.substringAfterLast("/")
         assertThat(surveyId).isNotEmpty()
 
         val expectedLocation = URI.create("${restTemplate.rootUri}/api/surveys/${surveyId}")
         assertThat(actualLocation).isEqualTo(expectedLocation)
 
+        // Check that newly created resource can be found from the expected location
         val getResponse = restTemplate.getForEntity(expectedLocation, String::class.java)
         assertThat(getResponse.statusCode).isEqualTo(HttpStatus.OK)
         assertThat(getResponse.body).isNotEmpty()
 
         val documentContext: DocumentContext = JsonPath.parse(getResponse.body)
 
+        // Create SurveyDTO object from actual data from the response object
         val actualSurvey = SurveyDTO(
             issueNumber = documentContext.read("$.issueNumber"),
             issueLink = documentContext.read("$.issueLink"),
@@ -119,7 +121,6 @@ class SurveyControllerIntegrationTests @Autowired constructor(
             taskTitle = documentContext.read("$.taskTitle"),
             instructions = documentContext.read("$.instructions"),
         )
-
         assertThat(newSurvey).isEqualTo(actualSurvey)
     }
 }
