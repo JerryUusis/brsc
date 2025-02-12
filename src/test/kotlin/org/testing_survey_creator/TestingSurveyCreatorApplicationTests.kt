4cecf548a41dd29ed60089472caf8c231769eb75
Jerry Uusitalo <jerry.uusitalo@gmail.com>
2025-02-12 13:54:50 +0200

    Remove unused code

--- src/test/kotlin/org/testing_survey_creator/TestingSurveyCreatorApplicationTests.kt
+++ src/test/kotlin/org/testing_survey_creator/TestingSurveyCreatorApplicationTests.kt
@@ -2,12 +2,8 @@ package org.testing_survey_creator
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
-import org.testcontainers.junit.jupiter.Testcontainers
 
-@Testcontainers
 @SpringBootTest
-@ActiveProfiles("test")
 class TestingSurveyCreatorApplicationTests {
 
     @Test
