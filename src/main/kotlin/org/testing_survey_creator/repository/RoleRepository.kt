17f0709671a18c0dd255a2ffcf8bc9ce9a586c02
Jerry Uusitalo <jerry.uusitalo@gmail.com>
2025-03-05 11:06:25 +0200

    Initiate GlobalExceptionHandler and exception for duplicate user.

--- src/main/kotlin/org/testing_survey_creator/repository/RoleRepository.kt
+++ src/main/kotlin/org/testing_survey_creator/repository/RoleRepository.kt
@@ -3,8 +3,9 @@ package org.testing_survey_creator.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import org.testing_survey_creator.model.Role
+import java.util.*
 
 @Repository
 interface RoleRepository : JpaRepository<Role, Long> {
-    fun findByName(name: String): Role
+    fun findByName(name: String): Optional<Role>
 }
--- src/main/kotlin/org/testing_survey_creator/service/UserService.kt
+++ src/main/kotlin/org/testing_survey_creator/service/UserService.kt
@@ -4,6 +4,7 @@ import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.testing_survey_creator.repository.UserRepository
 import org.testing_survey_creator.dto.UserRegistrationDTO
+import org.testing_survey_creator.exception.UserAlreadyExistsException
 import org.testing_survey_creator.model.User
 import org.testing_survey_creator.repository.RoleRepository
 import java.time.Instant
@@ -15,7 +16,12 @@ class UserService(
     private val passwordEncoder: PasswordEncoder
 ) {
     fun createUser(dto: UserRegistrationDTO): User {
-        val userRole = roleRepository.findByName("USER")
+        val userRole = roleRepository.findByName("USER").orElseThrow { IllegalStateException("Role not found") }
+
+        val existingUser = userRepository.findByEmail(dto.email).isPresent
+        if (existingUser) {
+            throw UserAlreadyExistsException("User already exists")
+        }
 
         val newUser = User(
             username = dto.username,
