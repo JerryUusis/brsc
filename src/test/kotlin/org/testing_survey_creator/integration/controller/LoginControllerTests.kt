1742f657b05e12a328da3f634c612ac0ba591a79
Jerry Uusitalo <jerry.uusitalo@gmail.com>
2025-03-06 14:51:54 +0200

    Add test case for succesful login

--- src/test/kotlin/org/testing_survey_creator/integration/controller/LoginControllerTests.kt
+++ src/test/kotlin/org/testing_survey_creator/integration/controller/LoginControllerTests.kt
@@ -1,5 +1,7 @@
 package org.testing_survey_creator.integration.controller
 
+import com.jayway.jsonpath.DocumentContext
+import com.jayway.jsonpath.JsonPath
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.assertj.core.api.Assertions.assertThat
@@ -11,7 +13,9 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.testing_survey_creator.dto.LoginDTO
+import org.testing_survey_creator.dto.UserRegistrationDTO
 import org.testing_survey_creator.repository.UserRepository
+import org.testing_survey_creator.security.JwtUtil
 import org.testing_survey_creator.util.AbstractIntegrationTest
 import java.net.URI
 
@@ -19,12 +23,35 @@ import java.net.URI
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class LoginControllerTests @Autowired constructor(
     private val restTemplate: TestRestTemplate,
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val jwtUtil: JwtUtil
 ) : AbstractIntegrationTest() {
 
     @BeforeEach
     fun setup() {
         userRepository.deleteAll()
+    }
+
+    @Test
+    fun `Successful login should return valid token`() {
+        val newUser = UserRegistrationDTO("testuser", "test@user.com", "testpassword")
+
+        val registrationResponse = restTemplate.postForEntity("/api/users", newUser, UserRegistrationDTO::class.java)
+        assertThat(registrationResponse.statusCode).isEqualTo(HttpStatus.CREATED)
+        assertThat(userRepository.findByEmail(newUser.email)).isPresent
+
+        val loginBody = LoginDTO(newUser.email, newUser.password)
+        val loginResponse = restTemplate.postForEntity("/api/login", loginBody, String::class.java)
+        assertThat(loginResponse.statusCode).isEqualTo(HttpStatus.OK)
+
+        val documentContext: DocumentContext = JsonPath.parse(loginResponse.body)
+        val token = documentContext.read<String>("$.token")
+
+        assertThat(token).startsWith("eyJhb")
+        assertThat(jwtUtil.isTokenValid(
+            token,
+            newUser.email,
+        )).isTrue
     }
 
     @Test
